b208ae6e3ecb1d1db44d0caba271f9c3376dc41e
Hau Chu <hchoward1998@gmail.com>
2024-03-15 20:37:32 -0400

    fix backend structure

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,25 @@
 import * as cors from "cors";
-import { ethers } from "ethers";
 import * as express from "express";
-import { errorHandler } from "./middleware/error";
 import * as winston from "winston";
 
-// const winston = require("winston");
+import { privateKeyToAccount } from "viem/accounts";
+import { StoryClient, StoryConfig } from "@story-protocol/core-sdk";
+import { Account, Transport, http } from "viem";
+import { AppDataSource } from "./data-source";
+import { Nft } from "./entity/Nft";
+import { Request, Response } from "express";
+
+const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY || "0x";
+const account = privateKeyToAccount(WALLET_PRIVATE_KEY as `0x${string}`);
+
+const config: StoryConfig = {
+  transport: http(
+    process.env.RPC_PROVIDER_URL || "https://rpc.ankr.com/eth_sepolia"
+  ),
+  account,
+};
+const client = StoryClient.newClient(config);
+
 const { combine, timestamp, printf, colorize, align } = winston.format;
 
 const app = express();
@@ -13,14 +28,15 @@ const app = express();
 app.set("trust proxy", 1);
 app.use(express.json()); // or use bodyParser.json()?
 
-// const corsOptions = {
-//   origin: "*",
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   credentials: true,
-//   optionsSuccessStatus: 204,
-// };
-app.use(cors());
-app.use(errorHandler);
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
+// app.use(errorHandler);
+// app.use(cors());
 
 // Set up logger
 
@@ -62,6 +78,129 @@ app.use((req, res, next) => {
     `method: ${req.method} url: ${req.url} body: ${body} status: ${res.statusCode}`
   );
   next();
+});
+
+app.get("/hello", (req, res) => {
+  return res.status(200).json({ message: "Hello, world!" });
+});
+
+app.post("/storyprotocol/registerip", async (req, res) => {
+  try {
+    const { contractAddress, tokenId } = req.body;
+    console.log(contractAddress, tokenId);
+    const response = await client.ipAsset.registerRootIp({
+      tokenContractAddress: contractAddress, // your NFT contract address
+      tokenId, // your NFT token ID
+      txOptions: { waitForTransaction: true },
+    });
+
+    console.log(
+      `Root IPA created at transaction hash ${response.txHash}, IPA ID: ${response.ipId}`
+    );
+    return res.status(200).json({ message: "Root IPA created successfully" });
+  } catch (e) {
+    console.error(e);
+    return res.status(400).json({ message: "Invalid Request" });
+  }
+});
+
+app.get("getNft/:chainId/:contractAddress/:tokenId", async (req, res) => {
+  try {
+    const { chainId, contractAddress, tokenId } = req.params;
+
+    const nftRepository = AppDataSource.getRepository(Nft);
+    const nft = await nftRepository.findOneBy({
+      chainId: parseInt(chainId),
+      contractAddress,
+      tokenId: parseInt(tokenId),
+    });
+
+    if (!nft) {
+      return res.status(404).json({ message: "NFT not found" });
+    }
+
+    return res.status(200).json({ nft });
+  } catch (e) {
+    console.error(e);
+    return res.status(400).json({ message: "Invalid " });
+  }
+});
+
+app.post("/createNft", async (req: Request, res: Response) => {
+  try {
+    const { walletAddress, chainId, contractAddress, tokenId, ipId } = req.body;
+
+    const nftRepository = AppDataSource.getRepository(Nft);
+    const nft = await nftRepository.findOneBy({
+      chainId: parseInt(chainId),
+      contractAddress,
+      tokenId: parseInt(tokenId),
+    });
+
+    if (nft) {
+      return res.status(400).json({ message: "NFT already exists" });
+    }
+
+    const newNft = nftRepository.create({
+      walletAddress,
+      chainId,
+      contractAddress,
+      tokenId,
+      ipId,
+    });
+
+    await nftRepository.save(newNft);
+
+    return res.status(201).json({ message: "NFT created successfully" });
+  } catch (e) {
+    console.error(e);
+    return res.status(400).json({ message: "Invalid " });
+  }
+});
+
+app.put("updateNft", async (req: Request, res: Response) => {
+  try {
+    const { id, ipId } = req.body;
+    const nftRepository = AppDataSource.getRepository(Nft);
+
+    const nft = await nftRepository.findOneBy({
+      id,
+    });
+
+    if (!nft) {
+      return res.status(400).json({ message: "NFT not found" });
+    }
+
+    nft.ipId = ipId;
+    await nftRepository.save(nft);
+
+    return res.status(200).json({ message: "NFT updated successfully" });
+  } catch (e) {
+    console.error(e);
+    return res.status(400).json({ message: "Invalid " });
+  }
+});
+
+app.delete("deleteNft", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.body;
+    const nftRepository = AppDataSource.getRepository(Nft);
+
+    const nft = await nftRepository.findOneBy({
+      id,
+    });
+
+    if (!nft) {
+      return res.status(400).json({ message: "NFT not found" });
+    }
+
+    await nftRepository.delete(id);
+
+    return res.status(200).json({ message: "NFT deleted successfully" });
+  } catch (e) {
+    console.error(e);
+    return res.status(400).json({ message: "Invalid " });
+  }
 });
 
 export default app;
--- backend/src/middleware/error.ts
+++
@@ -1,6 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export const errorHandler = (error, req, res, next) => {
-  console.error(error);
-  res.status(500).json({ message: "Internal server error" });
-};
